2ac7fe427d930e8c566287d679af5687c93e594b
ktvprasaad <47123069+ktvprasaad@users.noreply.github.com>
2020-06-02 15:25:17 -0400

    Link Update
    Link and scroll logic update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 const get = element => document.getElementById(element);
-// const query = element => document.getElementsByClassName(element);
 
 let open = get("menu-btn");
 let nav = get("nav");
@@ -18,75 +17,45 @@ exit.addEventListener('click', () => {
 	nav.classList.remove('open-nav');
 })
 
+hme.addEventListener('click', (event) => {
+	console.log(event.target);
+	nav.classList.remove('open-nav');
+})
+
 abt.addEventListener('click', (event) => {
 	console.log(event.target);
 	nav.classList.remove('open-nav');
-	abt.classList.add('active');
-	hme.classList.remove('active');
-	skl.classList.remove('active');
-	wrk.classList.remove('active');
-	cnt.classList.remove('active');
 })
-
 skl.addEventListener('click', (event) => {
 	console.log(event.target);
 	nav.classList.remove('open-nav');
-	abt.classList.remove('active');
-	hme.classList.remove('active');
-	skl.classList.add('active');
-	wrk.classList.remove('active');
-	cnt.classList.remove('active');
 })
 
 wrk.addEventListener('click', (event) => {
 	console.log(event.target);
 	nav.classList.remove('open-nav');
-	abt.classList.remove('active');
-	hme.classList.remove('active');
-	skl.classList.remove('active');
-	wrk.classList.add('active');
-	cnt.classList.remove('active');
 })
 
 cnt.addEventListener('click', (event) => {
 	console.log(event.target);
 	nav.classList.remove('open-nav');
-	abt.classList.remove('active');
-	hme.classList.remove('active');
-	skl.classList.remove('active');
-	wrk.classList.remove('active');
-	cnt.classList.add('active');
 })
 
+let navLinks = document.querySelectorAll("nav ul li a");
+let mainSections = document.querySelectorAll("section");
 
-// let mainNavLinks = document.querySelectorAll("a");
-// let mainSections = document.querySelectorAll("section");
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY - 70;
 
-// let lastId;
-// let cur = [];
-
-// // This should probably be throttled.
-// // Especially because it triggers during smooth scrolling.
-// // https://lodash.com/docs/4.17.10#throttle
-// // You could do like...
-// // window.addEventListener("scroll", () => {
-// //    _.throttle(doThatStuff, 100);
-// // });
-// // Only not doing it here to keep this Pen dependency-free.
-
-// window.addEventListener("scroll", event => {
-//   let fromTop = window.scrollY;
-
-//   mainNavLinks.forEach(link => {
-//     let section = document.querySelector(link.hash);
-
-//     if (
-//       section.offsetTop <= fromTop &&
-//       section.offsetTop + section.offsetHeight > fromTop
-//     ) {
-//       link.classList.add("active");
-//     } else {
-//       link.classList.remove("active");
-//     }
-//   });
-// });
+  navLinks.forEach(link => {
+	let section = document.querySelector(link.hash);
+    if (
+      section.offsetTop <= fromTop + 75 &&
+      section.offsetTop + section.offsetHeight > fromTop + 75
+    ) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+});
